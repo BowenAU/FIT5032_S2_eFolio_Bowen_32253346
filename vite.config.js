67e2d97c69056b18458de10118d1da81dca0e8a9
Bowen Tian <herebowen@gmail.com>
2024-10-18 21:43:37 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,18 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools()
   ],
+  base: mode === 'production' ? '/FIT5032_S2_eFolio_Bowen_32253346/' : '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': new URL('./src', import.meta.url).pathname
     }
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/FIT5032_S2_eFolio_Bowen_32253346/'
-  : '/'
-})
+  }
+}))
 
+
+ 
